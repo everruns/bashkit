2776e759f5211d6f4a639f1c9e7dc473ca976d50
Mykhailo Chalyi <mike@chaliy.name>
2026-06-12 10:07:49 +0000

    style(awk): format multi-subscript test to satisfy cargo fmt

--- crates/bashkit/src/builtins/awk/tests.rs
+++ crates/bashkit/src/builtins/awk/tests.rs
@@ -597,7 +597,9 @@ async fn test_awk_multi_subscript() {
 #[tokio::test]
 async fn test_awk_rejects_too_many_multi_subscripts() {
     // Regression: unbounded multi-subscript lists built a recursive SUBSEP_CONCAT AST.
-    let subscripts = std::iter::repeat_n("1", AWK_MAX_MULTI_SUBSCRIPTS + 1).collect::<Vec<_>>().join(",");
+    let subscripts = std::iter::repeat_n("1", AWK_MAX_MULTI_SUBSCRIPTS + 1)
+        .collect::<Vec<_>>()
+        .join(",");
     let program = format!("BEGIN {{ a[{subscripts}] = 1 }}");
 
     let err = run_awk(&[&program], Some("")).await.unwrap_err();
